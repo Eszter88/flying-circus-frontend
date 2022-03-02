2725e7de4ecfa6e08710d37ec45bf158759f9e8c
Eszter88 <eszterviva@gmail.com>
2022-03-02 09:47:23 +0000

    add finished EnglishText component

--- src/components/EnglishText.jsx
+++ src/components/EnglishText.jsx
@@ -1,4 +1,5 @@
 import loginGuard from "./loginGuard";
+import { useState } from "react";
 
 const Question = (props) => {
   return (
@@ -7,11 +8,24 @@ const Question = (props) => {
       <ul>
         {props.answers.map((answer, index) => {
           return props.activeIndex === index ? (
-            <li key={index} style={{ color: "blue" }}>
+            <li
+              key={index}
+              style={{ color: "blue" }}
+              onClick={() => {
+                props.onSelectAnswer(index);
+              }}
+            >
               {answer}
             </li>
           ) : (
-            <li key={index}>{answer}</li>
+            <li
+              key={index}
+              onClick={() => {
+                props.onSelectAnswer(index);
+              }}
+            >
+              {answer}
+            </li>
           );
         })}
       </ul>
@@ -21,14 +35,24 @@ const Question = (props) => {
 };
 
 const EnglishTest = () => {
-  const question = {
+  const [question, setQuestion] = useState({
     question: "question text",
     answers: ["answer1", "answer2", "answer3", "answer4"],
+  });
+
+  const [activeIndex, setActiveIndex] = useState(-1);
+
+  const onSelectAnswer = (index) => {
+    setActiveIndex(index);
   };
 
   return (
     <div>
-      <Question {...question} activeIndex={3} />
+      <Question
+        {...question}
+        activeIndex={activeIndex}
+        onSelectAnswer={onSelectAnswer}
+      />
     </div>
   );
 };
